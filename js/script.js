70f38109145d7a407f44192d3a2b61c30d2a4eac
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-06-15 00:13:48 +0200

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -18,6 +18,8 @@
 // cioè il numero di volte che l’utente ha scelto un numero che non era una bomba.
 
 // *FUNCTION*
+// *dichiaro una funzione che genera 16 numeri randomici e li inserisce in un array*
+// *una volta generati ritorno l'array che ne risulta*
 function bombsGenerator (bombs, maxValue) {
 
     let randomNumber;
@@ -37,7 +39,16 @@ function bombsGenerator (bombs, maxValue) {
 }
 
 function getRndInteger (min, max) {
+
     return Math.floor(Math.random() * (max - min + 1) ) + min;
+
+}
+
+// *dichiaro una funzione che determina se un numero è un decimale o meno*
+function isFloat(n) {
+
+    return n === +n && n !== (n|0);
+
 }
 
 // *VARIABLES*
@@ -45,32 +56,110 @@ let difficultyRangeNumber;
 
 let bombsCounter = 16;
 
-let gameDiffuclty = prompt("scegli il livello di difficoltà da 1 a 3");
+let gameDifficulty;
 
+// *dichiaro un array vuoto in cui slavare il numero di tentativi dell'utente*
+const triesArray = [];
+
+// *dichiaro una variabile "flag" che cambierà valore in base al risultato del gioco*
+let condition = true;
+
+// *variabile in cui salvo il valore del "return" della funzione "bombsGenerator"*
+let bombs;
+
+let userTries;
+
+// *dichiaro una variabile che cambierà in base a se l'utente vince o perde*
+let userMessage;
+
+// *dichiaro una variabile a cui attribuirò il punteggio ottenuto dall'utente a fine gioco*
+let userScore;
 
 // *LOGIC*
-switch (gameDiffuclty) {
+// *inizio un ciclo "while" per far scegliere all'utente la difficoltà del gioco e*
+// *controllo che il numero inserito sia un numero cmpreso tra 1 e 3*
+while (gameDifficulty < 1 || gameDifficulty > 3 || isNaN(gameDifficulty) || isFloat(gameDifficulty)) {
 
-    case "1":
+    gameDifficulty = parseFloat(prompt("Scegli il livello di difficoltà da 1 a 3"));
+
+    // !console.log per rendere più comprensibili i passaggi!
+    // console.log(gameDifficulty);
+
+    if (gameDifficulty === 1) {
         difficultyRangeNumber = 100;
-        break;
-    
-    case "2":
+        alert("Hai scelto il livello 1!");
+        alert("Buona fortuna!");
+    } else if (gameDifficulty === 2) {
         difficultyRangeNumber = 81;
-        break;
-
-    case "3":
+        alert("Hai scelto il livello 2!");
+        alert("Buona fortuna!");
+    } else if (gameDifficulty === 3) {
         difficultyRangeNumber = 49;
-        break;
-
-    default:
-        difficultyRangeNumber = 100;
+        alert("Hai scelto il livello 3! Se è troppo difficile refresha la pagina e scegli un livello di difficoltà inferiore.");
+        alert("Buona fortuna!");
+    }  else if ((isFloat(gameDifficulty))) {
+        alert("Attenzione! Non puoi inserire numeri decimali!");
+    } else if (isNaN(gameDifficulty)) {
+        alert("Puoi scrivere solo numeri!");
+    } else if (gameDifficulty < 1 || gameDifficulty > 3) {
+        alert("Numero errato!");
+    }
 }
 
-let bombsArray = bombsGenerator(bombsCounter, difficultyRangeNumber);
+// *numero di tentativi possibili per determinare la fine del gioco*
+userTries = difficultyRangeNumber - bombsCounter;
 
-console.log(bombsArray);
+// *salvo l'array ottenuto con la funzione "bombsGenerator"*
+// *nella variabile "bombs" precedentemente dichiarata*
+bombs = bombsGenerator(bombsCounter, difficultyRangeNumber);
 
-while () {
-    const
+// !console.log per rendere più comprensibili i passaggi!
+// console.log(bombs);
+
+// *inizio un ciclo "while" con all'interno tutte le condizioni per permettere al gioco di contnuare*
+while (condition) {
+
+    // *chiedo all'utente un numero compreso tra 1 e "difficultyRangeNumber"*
+    // *e salvo l'input ricevuto in una variabile "userNumber"*
+    let userNumber = parseInt(prompt(`inserisci un numero da 1 a ${difficultyRangeNumber}`));
+
+    // *se "userNumber" è presente nell'array slavato nella variabile "bombs"*
+    // *allora la variabile "condition" diventerà "false" e il ciclo "while" terminerà*
+    // *così come il gioco, dopodichè salverò in "userMessage" e "userScore" il relativo*
+    // *risultato e punteggio ottenuto*
+    if (bombs.includes(userNumber)) {
+        condition = false;
+        userMessage = "Hai perso!";
+        userScore = `Il tuo punteggio è: ${triesArray.length}`;
+    } else {
+        
+        // *altrimenti se il numero non è presente in "bombs" e tutte le condizioni di validazione sono "false"*
+        // *inserisco "userNumber" nell'array "triesArray"*
+        if (isNaN(userNumber)) {
+            alert("Puoi inserire solo numeri!");
+        } else if (userNumber < 1 || userNumber > difficultyRangeNumber) {
+            alert(`Attenzione! Non puoi inserire numeri minori di 1 o maggiori di ${difficultyRangeNumber}!`);
+        } else if (!triesArray.includes(userNumber) && !bombs.includes(userNumber)) {
+            triesArray.push(userNumber);
+        } else if (triesArray.includes(userNumber)) {
+            alert("Numero già inserito! Riprova!");
+        } 
+    }
+
+    // *se la lunghezza dell'array "triesArray" corrisponde a "userTries"*
+    // *la variabile "condition" diventerà "false" e il ciclo "while" terminerà*
+    // *così come il gioco, dopodichè salverò in "userMessage" e "userScore" il relativo*
+    // *risultato e punteggio ottenuto*
+    if (triesArray.length === userTries) {
+        condition = false;
+        userMessage = "Congratulazioni! Hai vinto!";
+        userScore = `Il tuo punteggio è: ${triesArray.length}!`;
+    }
+    
+    // !console.log per rendere più comprensibili i passaggi!
+    // console.log(triesArray);
 }
+
+// *stampo in pagina il risultato del gioco*
+alert(userMessage);
+alert(userScore);
